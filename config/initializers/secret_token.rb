3d2134ee27b5b7e0922095816e3f03648e99a3aa
Vikas Chandra <vikkycorner@gmail.com>
2015-07-27 17:06:15 +0530

    Done Scaffolding on Product and designed the table to show products

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OnlineShopping::Application.config.secret_token = '0f7e5e13a97c730493a84c65c3f8e595381d091897d254ba4d81bc20b409c3706b8f893d95b0879b13cfe550f96de2d7f0a362bd64c73805f20a8e2103a66e10'
