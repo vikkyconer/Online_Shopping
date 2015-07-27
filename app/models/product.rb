a5e03a0e74700b02424f25cca1cfbf463e2a5658
Vikas Chandra <vikkycorner@gmail.com>
2015-07-27 21:38:31 +0530

    catalog listing created

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ActiveRecord::Base
 	validates :title, :description, :image_url, presence: true
 	validates :price , numericality: {greater_than_or_equal_to: 0.01}
 	validates :title, uniqueness: true
+	validates :title, length: {minimum: 3}
 	validates :image_url, allow_blank: true , format: {
 		with: %r{\.(gif|jpg|png)$}i,
 		message: 'must be a url for gif , jpg or png' 
